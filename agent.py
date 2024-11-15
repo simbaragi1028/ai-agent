4a7a414d4b8f97672b140afc0dfc573abb2843bc
simbaragi1028 <parksm2419@naver.com>
2024-11-15 17:12:58 +0900

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
+import openai
+import os
+openai.api_key= os.environ.get("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")
